8f44385583ee433234c15da358bb0d1628e4bccd
iaredgallus <jareddhawn@gmail.com>
2024-07-05 19:28:40 -0500

    Updates to NaloGenerator folder.

--- NaloGenerator/nalo.py
+++ NaloGenerator/nalo.py
@@ -1,9 +1,30 @@
-class Dog:
+class Word:
     type = ''
-    color = ''
+    words = []
+    subjects = []
+    objects = []
 
-chihuahua = Dog()
-chihuahua.type = 'chihuahua'
-chihuahua.color = 'black'
+pronoun = Word()
+pronoun.words = ['I', 'you']
 
-println(chihuahua)
+animals = Word()
+animals.words = ['cat', 'dog', 'mouse']
+
+house = Word()
+house.words = ['table', 'chair', 'couch', 'wall']
+
+verb = Word()
+verb.type = 'verb'
+verb.words = ['talk', 'walk', 'hit']
+verb.subjects = [pronoun]
+verb.objects = [animals]
+
+sentenceverb = verb.words[1]
+sentencepronoun = verb.subjects[0].words[0]
+sentenceobject = verb.objects[0].words[1]
+
+sentence = sentencepronoun + ' ' + sentenceverb + ' ' + sentenceobject + '.'
+
+print(sentence)
+
+# print('what the heck')
