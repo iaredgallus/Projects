e188019e72205e3182efe5e33151973f9b42f526
iaredgallus <jareddhawn@gmail.com>
2025-02-20 17:19:02 -0600

    A functional app with calls to spotify API to get and post. Credentials are stored in JS files so not entirely secure.

--- playlist-builder/src/App.js
+++ playlist-builder/src/App.js
@@ -1,56 +1,108 @@
 //import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-import SearchByAlbum from './SearchByAlbum';
+//import SearchByAlbum from './SearchByAlbum';
 import SearchByTrack from './SearchByTrack';
+let isLoggedIn = false;
 const clientId = '9eecd97b0c1f48feae4d1917c29c5a0d';
 const clientSecret = 'b73a12900a264892a605ca6c9417964f';
-// Extract Auth Code if it exists.
+const authString = btoa(`${clientId}:${clientSecret}`);
+const redirectUri = 'http://localhost:3000'; // THIS NEEDS TO BE UPDATED IF PUTTING ON SERVER
+let accessToken = null;
+let refreshToken = null;
+
+// EXTRACT TOKENS BEFORE LOADING APP
 const urlParams = new URLSearchParams(window.location.search);
 let getCode = urlParams.get("code");
-let isLoggedIn = false;
+
 if (getCode !== null) {
-    isLoggedIn = true;
+  isLoggedIn = true;
+  window.history.replaceState({}, document.title, window.location.pathname); // This brings the URL back to normal
 }
 
+const fetchBody = new URLSearchParams({
+  grant_type: "authorization_code",
+  code: getCode,
+  redirect_uri: redirectUri
+});
+
+if (isLoggedIn) {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Authorization": `Basic ${authString}`,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: fetchBody
+  });
+  
+  const data = await response.json();
+  accessToken = data.access_token;
+  refreshToken = data.refresh_token;
+  if (accessToken !== null && refreshToken !== null) {
+    console.log("accessToken and refreshToken obtained");
+  }
+}
+
+/* BEGIN APP COMPONENT */
+
 function App() {
-  const [ albumVisible, setAlbumVisible ] = useState(false);
+  //const [ albumVisible, setAlbumVisible ] = useState(false);
   const [ trackVisible, setTrackVisible ] = useState(true);
   const [ loggedIn, setLoggedIn ] = useState(false);
-  const [ authCode, setAuthCode ] = useState(null);
-  const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost:3000&scope=playlist-modify-public&state=random_string`;
+  //const [ authCode, setAuthCode ] = useState(null);
+  const [ postAccessToken, setPostAccessToken ] = useState(null);
+  const [ postRefreshToken, setPostRefreshToken ] = useState(null);
+  const [ givenPassword, setGivenPassword ] = useState("");
+  const [ allowed, setAllowed ] = useState(false);
+  const [ badPassword, setBadPassword ] = useState(false);
+  const password = "recommend!";
+  const appRedirectUri = redirectUri;
+  const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=playlist-modify-public&state=random_string`;
 
   useEffect(() => {
     if (getCode !== null) {
       setLoggedIn(true);
-      setAuthCode(getCode);
+      //setAuthCode(getCode);
+      setPostAccessToken(accessToken);
+      setPostRefreshToken(refreshToken);
     }
   });
 
-  function toggleAlbum() {
-    setAlbumVisible(!albumVisible);
+  function handleGivenPassword(e) {
+    e.preventDefault();
+    setGivenPassword(e.target.value);
   }
-  function toggleTrack() {
-    setTrackVisible(!trackVisible);
+
+  function handleTryPassword(e) {
+    e.preventDefault();
+    if (givenPassword === password) {
+      setAllowed(true);
+      setBadPassword(false);
+    } else {
+      setBadPassword(true);
+    }
   }
 
   return (
     <div className="App">
         <h2>Search for a Song</h2>
-        <div className={(loggedIn) ? "hidden" : ""}><a href={loginUrl}>Log in to Spotify to Add Songs</a></div>
         <div className={trackVisible ? "" : "hidden"}>
-          <SearchByTrack loggedIn={loggedIn} authCode={authCode}/>
+          <SearchByTrack loggedIn={loggedIn} postAccessToken={postAccessToken} postRefreshToken={postRefreshToken} />
+        </div>
+        <div style={{marginTop: "4rem"}} className={(loggedIn) ? "hidden" : ""}>
+          <hr />
+          <form onSubmit={handleTryPassword} className={(allowed) ? "hidden" : ""}>
+            <div>If you want to add a song to my Recommendations playlist, you'll need to ask me for the password.</div>
+            <div>Enter password below.</div>
+            <input id="password" value={givenPassword} onChange={handleGivenPassword} />
+            <button type="submit">Enter</button>
+          </form>
+          <div className={(badPassword) ? "" : "hidden"}>That didn't work.</div>
+          <div className={(loggedIn || !allowed) ? "hidden" : ""}>That was it!<br />Next, <a href={loginUrl}>connect to Spotify</a> to add songs directly to my Recommendations playlist.<br />(You're using my API tokens, so be nice.)</div>
         </div>
     </div>
   );
 }
-
-/*
-        <button onClick={toggleAlbum}>By Album</button>
-        <div className={albumVisible ? "" : "hidden"}>
-          <SearchByAlbum />
-        </div>
-        <button onClick={toggleTrack}>By Track</button>
-*/
 
 export default App;
--- playlist-builder/src/SearchByTrack.js
+++ playlist-builder/src/SearchByTrack.js
@@ -6,7 +6,7 @@ const clientId = '9eecd97b0c1f48feae4d1917c29c5a0d';
 const clientSecret = 'b73a12900a264892a605ca6c9417964f';
 const playlistId = '3DvVwlxBtWk8dikEbWTlV2';
 const result = await getToken();
-const accessToken = result.access_token;
+const getAccessToken = result.access_token;
 const noBullets = { listStyle: "none" }; // CSS
 
 async function getToken() {
@@ -23,106 +23,133 @@ async function getToken() {
   return response.json();
 }
 
-async function searchForTrack(accessToken, selectedTrack) {
+async function searchForTrack(getAccessToken, selectedTrack) {
     const response = await fetch(`https://api.spotify.com/v1/search?q=${selectedTrack}&type=track`, {
       method: 'GET',
-      headers: { 'Authorization': 'Bearer ' + accessToken },
+      headers: { 'Authorization': 'Bearer ' + getAccessToken },
     });
   
     return await response.json();
 }
 
-async function getTrackInfo(accessToken, selectedTrackHref) {
+async function getTrackInfo(getAccessToken, selectedTrackHref) {
     const response = await fetch(selectedTrackHref, {
       method: 'GET',
-      headers: { 'Authorization': 'Bearer ' + accessToken },
+      headers: { 'Authorization': 'Bearer ' + getAccessToken },
     });
     
     return await response.json();
 }
 
-async function pushToPlaylist(accessToken, selectedTrack) {
-    const testTrack = '05LvE6ySn6hEcHdAJ7N19z';
-    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-        method: 'POST',
-        headers: { 'Authorization': 'Bearer ' + accessToken, 'Content-Type': 'application/json' },
-        data: { 'uris': [`spotify:track:${testTrack}`]}
-    });
-    //console.log('Done.');
-    return await response.json();
+async function getPlaylistTracks() {
+  const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+    method: 'GET',
+    headers: { 'Authorization': 'Bearer ' + getAccessToken }
+  });
+
+  return await response.json();
 }
 
-async function testToken(accessToken) {
-    const response = await fetch("https://api.spotify.com/v1/me", {
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + accessToken}
-    });
-    return await response.json();
+async function playlistToArray() {
+  // json.tracks.items[i].track.id
+  const trackList = [];
+  const json = await getPlaylistTracks();
+  const tracks = json.tracks.items;
+  for (let t of tracks) {
+    trackList.push(t.track.id);
+  }
+  return trackList || [];
+}
+
+async function pushToPlaylist(postAccessToken, selectedTrack) {
+  const trackId = selectedTrack.split("tracks/")[1];
+  const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+      method: 'POST',
+      headers: { 'Authorization': 'Bearer ' + postAccessToken, 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        uris: [`spotify:track:${trackId}`]})
+  });
+  return await response.json();
 }
 
 function SearchByTrack(props) {
     const [ track, setTrack ] = useState('');
     const [ titles, setTitles ] = useState([]);
-    //const [ artists, setArtists ] = useState([]);
     const [ selectedTrack, setSelectedTrack ] = useState('');
     const [ trackInfo, setTrackInfo ] = useState({});
     const [ trackInfoVisible, setTrackInfoVisible ] = useState(false);
-    //console.log(props.authCode);
+    const [ success, setSuccess ] = useState(false);
+    const [ alreadyOnPlaylist, setAlreadyOnPlaylist ] = useState(false);
+    const postAccessToken = props.postAccessToken;
     
     function handleChangeTrack(e) {
-        setTrack(e.target.value);
+      setTrack(e.target.value);
     }
 
     function handleSelectTrack(e) {
-        e.preventDefault();
+      e.preventDefault();
+      const currentTrack = selectedTrack;
+      if (currentTrack !== e.currentTarget.getAttribute("data-value")) {
         setSelectedTrack(e.currentTarget.getAttribute("data-value"));
+        setSuccess(false);
+        setAlreadyOnPlaylist(false);
+      }
     }
 
     function handleAddToPlaylist(e) {
-        e.preventDefault();
-        //pushToPlaylist(accessToken, selectedTrack);
+      e.preventDefault();
+      const trackId = selectedTrack.split("tracks/")[1];
+      let alreadyExists = false;
+      playlistToArray().then(array => {
+        for (let i of array) {
+          if (i === trackId) {
+            alreadyExists = true;
+            setAlreadyOnPlaylist(true);
+            break;
+          }
+        }
+        if (!alreadyExists) {
+          const response = pushToPlaylist(postAccessToken, selectedTrack);
+          setSuccess(true);
+          setAlreadyOnPlaylist(false);
+        }
+      });
     }
 
     function handleSubmit(e) {
-        e.preventDefault();
-        if (track === undefined || track === '') {
-            console.log("No data entered.");
-            return;
-        }
-        searchForTrack(accessToken, track).then(profile => {
-            setTitles([]);
-            // profile.tracks.items = an array of Tracks
-            // profile.albums.items[0].name == Song Title
-            // profile.tracks.items[0].artists[0].name == Artist Name
-            // profile.albums.items.release_date == YYYY.MM.dd
-            const r = profile.tracks.items;
-            //console.log(r);
-            let numResults = 5;
-            for (let i = 0; i < numResults; i++) {
-              //console.log(`"${r[i].name}" - ${r[i].artists[0].name}, ${r[i].album.release_date.substring(0,4)}`);
-              setTitles(titles => [...titles, [r[i].name, r[i].artists[0].name, r[i].album.release_date.substring(0,4), r[i].href]]);
-              //console.log(r[i].href);
-            }
-            //console.log(titles);
-          });
+      e.preventDefault();
+      setSuccess(false);
+      if (track === undefined || track === '') {
+          console.log("No data entered.");
+          return;
+      }
+      searchForTrack(getAccessToken, track).then(profile => {
+          setTitles([]);
+          // profile.tracks.items = an array of Tracks
+          // profile.albums.items[0].name == Song Title
+          // profile.tracks.items[0].artists[0].name == Artist Name
+          // profile.albums.items.release_date == YYYY.MM.dd
+          const r = profile.tracks.items;
+          let numResults = 5;
+          for (let i = 0; i < numResults; i++) {
+            setTitles(titles => [...titles, [r[i].name, r[i].artists[0].name, r[i].album.release_date.substring(0,4), r[i].href]]);
+          }
+        });
     }
 
     useEffect(() => {
-        if (!selectedTrack) return;
-        //console.log(selectedTrack);
-        setTrackInfoVisible(true);
-        getTrackInfo(accessToken, selectedTrack).then(profile => {
-            setTrackInfo({
-                "name": profile.name,
-                "artist": profile.artists[0].name,
-                "date": profile.album.release_date.substring(0,4),
-                "image": profile.album.images[0].url,
-                "album": profile.album.name
-            });
-        });
+      if (!selectedTrack) return;
+      setTrackInfoVisible(true);
+      getTrackInfo(getAccessToken, selectedTrack).then(profile => {
+          setTrackInfo({
+            "name": profile.name,
+            "artist": profile.artists[0].name,
+            "date": profile.album.release_date.substring(0,4),
+            "image": profile.album.images[0].url,
+            "album": profile.album.name
+          });
+      });
     }, [selectedTrack]);
-
-    const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost:3000&scope=playlist-modify-public&state=random_string`;
 
     return (
         <>
@@ -135,36 +162,21 @@ function SearchByTrack(props) {
                     <li style={noBullets} key={index} data-value={item[3]} onClick={handleSelectTrack}>{item[0]} - {item[1]} ({item[2]})</li>
                 ))}
             </ul>
-            <div id="track_info" className={(trackInfoVisible) ? "" : "hidden"}>
+            <div id="track_info" className={(trackInfoVisible) ? "" : "hidden"} style={{marginTop: "2rem"}}>
+                <hr />
                 <img src={trackInfo.image} height={128} alt="Album Cover"/>
                 <div>{trackInfo.name}</div>
                 <div style={{fontStyle:"italic"}}>{trackInfo.album}</div>
                 <div>{trackInfo.artist}</div>
                 <div>{trackInfo.date}</div>
-                <button onClick={handleAddToPlaylist} className={(props.loggedIn) ? "" : "hidden"}>Add to Playlist</button>
+                <div id="actions" style={{marginTop:"1rem"}}>
+                  <button onClick={handleAddToPlaylist} className={(props.loggedIn && !success && !alreadyOnPlaylist) ? "" : "hidden"}>Add to Playlist</button>
+                  <div className={(success) ? "" : "hidden"}>Nice! Track added.</div>
+                  <div className={(alreadyOnPlaylist) ? "" : "hidden"}>Someone has already recommended this track.</div>
+                </div>
             </div>
         </>
     )
 }
-
-// <div className={(props.loggedIn) ? "hidden" : ""}><a href={loginUrl}>Log in to Spotify to Add Songs</a></div>
-
-/*
-async function getTrackInfo(accessToken) {
-  const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + accessToken },
-  });
-  
-  return await response.json();
-}
-
-getTrackInfo(accessToken).then(profile => {
-  //console.log(profile);
-  //console.log(profile.name, "-", profile.album.artists[0].name);
-  // profile.name = Song Title
-  // profile.album.artists[0].name = Artist
-});
-*/
 
 export default SearchByTrack;
